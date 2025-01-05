f8fe148476261f47c3a0f8c70b17a7e943221dc2
AlexeyP_QA <a.m.pozdnyakov.qa@gmail.com>
2025-01-05 15:44:13 +0300

    check add product

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductPage;
+import java.time.Duration;
+
+public class BaseTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    ProductPage productPage;
+    CartPage cartPage;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver);
+        productPage = new ProductPage(driver);
+        cartPage = new CartPage(driver);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
